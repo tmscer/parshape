fbd8cb513e3e4b55f53cba98cea23b48071a0b4e
Tomáš Černý <tmscer@gmail.com>
2022-07-30 12:10:07 +0200

    Extract `useOnCanvasClick` from `<Editor />`

--- components/Editor.js
+++ components/Editor.js
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 
 import useHistory from "../hooks/useHistory";
 import useLines from "../hooks/useLines";
+import useOnCanvasClick from "../hooks/useOnCanvasClick";
 import Canvas from "./Canvas";
 import EdgeButton from "./EdgeButton";
 import Parshape from "./Parshape";
@@ -123,63 +124,6 @@ export default function Editor() {
       <Parshape width={getHsizePt(settings.hsize)} lines={lines} />
     </div>
   );
-}
-
-function useOnCanvasClick({
-  newType,
-  newObject,
-  clearNew,
-  setNewObject,
-  addObject,
-}) {
-  const onCanvasClick = useCallback(
-    ({ x, y }) => {
-      if (!newType) {
-        return;
-      }
-
-      if (newType === "line") {
-        if (!newObject) {
-          setNewObject({
-            type: "line",
-            start: [x, y],
-            stop: [x, y],
-          });
-        } else {
-          const finalLine = {
-            ...newObject,
-            stop: [x, y],
-          };
-
-          addObject(finalLine);
-          clearNew();
-        }
-      } else if (newType === "circle") {
-        if (!newObject) {
-          setNewObject({
-            type: "circle",
-            center: [x, y],
-            // Some value to be updated later
-            radius: 10,
-          });
-        } else {
-          const dx = x - newObject.center[0];
-          const dy = y - newObject.center[1];
-
-          const finalCircle = {
-            ...newObject,
-            radius: Math.sqrt(dx * dx + dy * dy),
-          };
-
-          addObject(finalCircle);
-          clearNew();
-        }
-      }
-    },
-    [newType, newObject, clearNew, setNewObject, addObject]
-  );
-
-  return onCanvasClick;
 }
 
 function useOnCanvasMouseOver({ newType, setNewObject }) {
---
+++ hooks/useOnCanvasClick.js
@@ -0,0 +1,58 @@
+import { useCallback } from "react";
+
+export default function useOnCanvasClick({
+  newType,
+  newObject,
+  clearNew,
+  setNewObject,
+  addObject,
+}) {
+  const onCanvasClick = useCallback(
+    ({ x, y }) => {
+      if (!newType) {
+        return;
+      }
+
+      if (newType === "line") {
+        if (!newObject) {
+          setNewObject({
+            type: "line",
+            start: [x, y],
+            stop: [x, y],
+          });
+        } else {
+          const finalLine = {
+            ...newObject,
+            stop: [x, y],
+          };
+
+          addObject(finalLine);
+          clearNew();
+        }
+      } else if (newType === "circle") {
+        if (!newObject) {
+          setNewObject({
+            type: "circle",
+            center: [x, y],
+            // Some value to be updated later
+            radius: 10,
+          });
+        } else {
+          const dx = x - newObject.center[0];
+          const dy = y - newObject.center[1];
+
+          const finalCircle = {
+            ...newObject,
+            radius: Math.sqrt(dx * dx + dy * dy),
+          };
+
+          addObject(finalCircle);
+          clearNew();
+        }
+      }
+    },
+    [newType, newObject, clearNew, setNewObject, addObject]
+  );
+
+  return onCanvasClick;
+}
