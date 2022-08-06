1eb96365da80e88213f54e7ccb3fb2630c2f1484
Tomáš Černý <tmscer@gmail.com>
2022-08-06 11:24:04 +0200

    Sample change to trigger CI

--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,5 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// `createJestConfig` is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
