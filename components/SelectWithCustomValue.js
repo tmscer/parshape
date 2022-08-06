7fcc978c004065f689982939a611bd4ae7a184da
Tomáš Černý <tmscer@gmail.com>
2022-08-06 12:27:51 +0200

    Add `<SelectWithCustomValue />`

---
+++ components/SelectWithCustomValue.js
@@ -0,0 +1,84 @@
+import ClearIcon from "@mui/icons-material/Clear";
+import { IconButton, InputAdornment, MenuItem, TextField } from "@mui/material";
+import { useCallback } from "react";
+
+const CUSTOM_VALUE = "__custom_value__";
+
+export default function SelectWithCustomValue({
+  value: { value, custom },
+  setValue: _setValue,
+  options,
+  ...props
+}) {
+  const getLabel = useCallback(
+    (value) => options.find((option) => option.value === value)?.label,
+    [options]
+  );
+
+  const setCustom = useCallback(
+    (value = 0) => {
+      _setValue({ value, custom: true });
+    },
+    [_setValue]
+  );
+
+  const setPredefinedValue = useCallback(
+    (value) => {
+      _setValue({ value, label: getLabel(value), custom: false });
+    },
+    [getLabel, _setValue]
+  );
+
+  const setValue = useCallback(
+    (e) => {
+      const newValue = e.target.value;
+
+      if (custom) {
+        setCustom(newValue);
+        return;
+      }
+
+      if (newValue === CUSTOM_VALUE) {
+        setCustom();
+        return;
+      }
+
+      setPredefinedValue(newValue);
+    },
+    [setCustom, setPredefinedValue, custom]
+  );
+
+  return (
+    <TextField
+      select={!custom}
+      value={value}
+      onChange={setValue}
+      InputProps={{
+        endAdornment: custom ? (
+          <ClearCustomValue
+            onClick={() => setPredefinedValue(options[0].value)}
+          />
+        ) : null,
+        ...(props.InputProps || {}),
+      }}
+      {...props}
+    >
+      {options.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+      ))}
+      <MenuItem value={CUSTOM_VALUE}> Custom</MenuItem>
+    </TextField>
+  );
+}
+
+function ClearCustomValue({ onClick }) {
+  return (
+    <InputAdornment position="end">
+      <IconButton onClick={onClick}>
+        <ClearIcon />
+      </IconButton>
+    </InputAdornment>
+  );
+}
