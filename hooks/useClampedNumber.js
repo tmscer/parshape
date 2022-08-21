5334bb7d91fbc30d9e692cd801d92532c1da4443
Tomáš Černý <tmscer@gmail.com>
2022-08-21 17:31:27 +0200

    Simplify implementation of `useClampedNumber`

--- hooks/useClampedNumber.js
+++ hooks/useClampedNumber.js
@@ -1,13 +1,8 @@
 import { clamp } from "lodash";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 export default function useClampedNumber(defaultValue, minValue, maxValue) {
-  const clamper = useCallback(
-    (value) => clamp(value, minValue, maxValue),
-    [minValue, maxValue]
-  );
-
   const [value, setValue] = useState(defaultValue);
 
-  return [clamper(value), setValue];
+  return [clamp(value, minValue, maxValue), setValue];
 }
