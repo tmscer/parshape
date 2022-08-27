1d58da5a88a5b99b1d0a8b0c73419f086d7c9506
Tomáš Černý <tmscer@gmail.com>
2022-08-27 12:38:17 +0200

    Submit `<ImportParshape>` by pressing `ctrl+enter`

--- components/ImportParshape.js
+++ components/ImportParshape.js
@@ -7,6 +7,7 @@ import {
   DialogTitle,
   Stack,
 } from "@mui/material";
+import isHotkey from "is-hotkey";
 import { useCallback, useMemo, useState } from "react";
 
 import parseParshape from "../utils/parseParshape";
@@ -32,6 +33,26 @@ export default function ImportParshape({ setLines, settings }) {
     }
   }, [parshape, setLines, settings.hsize]);
 
+  const onSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      onConfirm();
+    },
+    [onConfirm]
+  );
+
+  const onFormKeyDown = useCallback(
+    (event) => {
+      if (!isHotkey("ctrl+enter", event)) {
+        return;
+      }
+
+      onConfirm();
+    },
+    [onConfirm]
+  );
+
   return (
     <Stack direction="column">
       <Button variant="outlined" onClick={() => setOpen(true)}>
@@ -41,45 +62,51 @@ export default function ImportParshape({ setLines, settings }) {
         <DialogTitle>
           Import an existing <tt>\parshape</tt> command
         </DialogTitle>
-        <DialogContent>
-          <Alert severity="info" sx={{ mb: 2 }}>
-            Importing supports limited TeX syntax. For example groups
-            aren&apos;t supported but units listed{" "}
-            <a
-              href="https://en.wikibooks.org/wiki/TeX/TeX_dimensions"
-              target="_blank"
-              rel="noreferrer"
-            >
-              here
-            </a>{" "}
-            except <tt>em</tt> and <tt>ex</tt> are. Outputs from this project
-            will always be supported.
-          </Alert>
-          {error && (
-            <Alert severity="error" sx={{ mb: 2 }}>
-              {error.message}
+        <form
+          style={{ margin: 0, padding: 0 }}
+          onSubmit={onSubmit}
+          onKeyDown={onFormKeyDown}
+        >
+          <DialogContent>
+            <Alert severity="info" sx={{ mb: 2 }}>
+              Importing supports limited TeX syntax. For example groups
+              aren&apos;t supported but units listed{" "}
+              <a
+                href="https://en.wikibooks.org/wiki/TeX/TeX_dimensions"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>{" "}
+              except <tt>em</tt> and <tt>ex</tt> are. Outputs from this project
+              will always be supported.
             </Alert>
-          )}
-          <textarea
-            style={{ width: "100%", resize: "vertical" }}
-            value={parshape}
-            rows={`${Math.max(numTextLines + 5, 5)}`}
-            spellCheck={false}
-            onChange={(e) => setParshape(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button color="error" onClick={() => setOpen(false)}>
-            Cancel
-          </Button>
-          <Button
-            color="success"
-            variant="contained"
-            onClick={() => onConfirm()}
-          >
-            Confirm
-          </Button>
-        </DialogActions>
+            {error && (
+              <Alert severity="error" sx={{ mb: 2 }}>
+                {error.message}
+              </Alert>
+            )}
+            <textarea
+              style={{ width: "100%", resize: "vertical" }}
+              value={parshape}
+              rows={`${Math.max(numTextLines + 5, 5)}`}
+              spellCheck={false}
+              onChange={(e) => setParshape(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button color="error" onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+            <Button
+              color="success"
+              variant="contained"
+              onClick={() => onConfirm()}
+            >
+              Confirm
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </Stack>
   );
