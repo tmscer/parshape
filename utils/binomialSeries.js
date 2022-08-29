d9f349aef347468d528960f06ce56011ae2c7984
Tomáš Černý <tmscer@gmail.com>
2022-08-29 05:47:55 +0200

    Don't use `range` when calculating a binomial series
    Use a plain `Array.map` is simpler.

--- utils/binomialSeries.js
+++ utils/binomialSeries.js
@@ -1,10 +1,7 @@
 import binomialCoeffs from "./binomialCoeffs";
-import range from "./range";
 
 export default function binomialSeries(n, a, b) {
-  const coeffs = binomialCoeffs(n);
-
-  return range(0, n + 1).map(
-    (i) => coeffs[i] * Math.pow(a, n - i) * Math.pow(b, i)
+  return binomialCoeffs(n).map(
+    (coeff, i) => coeff * Math.pow(a, n - i) * Math.pow(b, i)
   );
 }
